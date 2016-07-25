329e1deb5d8029d95d77d5f3d1aea9d89d9ebf31
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-25 11:19:41 -0400

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name,breed,age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age
+}
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age
+}
+
+function Ferret(name,age){
+  this.name = name;
+  this.age = age
+  this.foodRequirements = 'raw meat'
+}
+
+function Bird(name, breed){
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name, species){
+  this.name = name
+  this.species = species
+  this.waterConditions = 'salt water'
+}
